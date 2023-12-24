684848e03f59209c17c2d98fb821c0ddaefb70b7
Sujay <sujaydey641@gmail.com>
2023-12-24 16:46:29 +0530

    nit

--- websites/settings.py
+++ websites/settings.py
@@ -19,10 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a1v*f0ub&gdkap8&83mkizp@q#lb(1)l!m21up$tt2s=3)cjt0'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['https://query-subtitle.azurewebsites.net/']
